969f7cb6029ff6ac9bf03a6cc660b16c1ae844f7
Diogo Toloto <diogogabriel0175@gmail.com>
2024-11-05 22:36:39 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+    return (
+      <>
+       <div className="bg-red-500 text-white p-4">Hello, Tailwind!</div>
+      </>
+    );
+  }
+  
+  export default App;
